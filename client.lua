86f9c94c5cc251f6115461e4e6f76a0590981872
ItsANoBrainer <itsanobrainergg@protonmail.com>
2022-01-14 11:27:24 -0500

    Promise implementation
    Now uses promises instead of silly loop!

--- client.lua
+++ client.lua
@@ -1,10 +1,9 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local function HasPhone()
-    local _hasPhone = nil
-    QBCore.Functions.TriggerCallback('QBCore:HasItem', function(hasItem) _hasPhone = hasItem end, Config.PhoneList)
-    while _hasPhone == nil do Wait(100) end
-    return _hasPhone
+    local p = promise.new()
+    QBCore.Functions.TriggerCallback('QBCore:HasItem', function(hasItem) p:resolve(hasItem) end, Config.PhoneList)
+    return Citizen.Await(p)
 end
   
 exports("HasPhone", HasPhone)
